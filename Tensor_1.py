92358bfe0e8121f6437146f11532abde9e16eebc
H Lalramenga <32388233+ramenga@users.noreply.github.com>
2017-11-30 22:03:05 +0530

    Add files via upload

---
+++ Tensor_1.py
@@ -0,0 +1,29 @@
+scores = [3.0, 1.0, 0.2]
+
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    pass  
+    i=0.0
+    T=0
+    length=0
+    softm=[]
+    for i in x:
+        T+=i
+        length+=1
+    
+    for i in range(0,length):
+        softm=softm+[(x[i]/T)]
+    return softm
+    
+
+print(softmax(scores))
+
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
